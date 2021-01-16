ae21a94114f68129abee8828261a4316e65bb99a
onurokkyay <59628395+onurokkyay@users.noreply.github.com>
2021-01-16 23:58:44 +0300

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace onurokkyay\Shopping;
+namespace onurokkyay\shopping;
 
 /**
  * Shopping module definition class
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * {@inheritdoc}
      */
-    public $controllerNamespace = 'onurokkyay\Shopping\controllers';
+    public $controllerNamespace = 'onurokkyay\shopping\controllers';
 
     /**
      * {@inheritdoc}
